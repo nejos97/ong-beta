9a7e18fe5189d6c51fa57e2b93f12d170cc93a28
Nenba <nenbajonathan@protonmail.com>
2019-04-02 12:45:57 +0100

    add whitenoise for styling

--- ong/prod_settings.py
+++ ong/prod_settings.py
@@ -1,5 +1,12 @@
 from ong.settings import *
+import dj_database_url
 
 TEMPLATE_DEBUG = False
 
 SECRET_KEY = "xa@%e5&m449$0($##&*hcywjib5(t_u)r*gt5dtc3@pjlfls8h"
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
